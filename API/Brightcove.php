fe854105d0a2dfab903e8cb718f09c0370bcea9e
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-05-26 15:56:16 -0500

    brightcovemapi wrapper

---
+++ API/Brightcove.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LCM\BrightcoveBundle\API;
+
+use BCAPI as BrightcoveBase;
+
+/**
+ * Wrapper for the brightcove api class
+ * @author jspizziri <jacob.spizziri@gmail.com>
+ */
+class Brightcove extends BrightcoveBase {
+
+  /**
+   * Alias find methods
+   */
+  public function __call($method, $args) {
+    if (strlen($method) > 4 && substr($method, 0,4) == 'find') {
+
+      $rest = substr($method, 4, strlen($method) - 1);
+      array_unshift($args, strtolower($rest));
+
+      return call_user_func_array('find', $args);
+    }
+  }
+}
