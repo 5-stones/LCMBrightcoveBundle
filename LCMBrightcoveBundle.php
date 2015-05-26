e7a369cd6e4dc4d81010cce00132f567b91d7506
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-05-26 11:31:08 -0500

    initial commit

---
+++ LCMBrightcoveBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LCM\BrightcoveBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LCMBrightcoveBundle extends Bundle
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LCM\BrightcoveBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
