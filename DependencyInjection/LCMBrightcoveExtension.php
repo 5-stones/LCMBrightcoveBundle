f893979a5f6f65d72db205f7621b0207612762f8
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-05-27 16:09:40 -0500

    update service definition

--- DependencyInjection/LCMBrightcoveExtension.php
+++ DependencyInjection/LCMBrightcoveExtension.php
@@ -26,12 +26,6 @@ class LCMBrightcoveExtension extends Extension
         $container->setParameter('lcm_brightcove.client.id', $config['client']['id']);
         $container->setParameter('lcm_brightcove.client.secret', $config['client']['secret']);
 
-        // $container->setParameter('lcm_brightcove.cache.type', isset($config['cache']) && $config['cache']['type'] ? $config['cache']['type'] : null);
-        // $container->setParameter('lcm_brightcove.cache.time', isset($config['cache']) && $config['cache']['time'] ? $config['cache']['time'] : null);
-        // $container->setParameter('lcm_brightcove.cache.location', isset($config['cache']) && $config['cache']['location'] ? $config['cache']['location'] : null);
-        // $container->setParameter('lcm_brightcove.cache.extension', isset($config['cache']) && $config['cache']['extension'] ? $config['cache']['extension'] : null);
-        // $container->setParameter('lcm_brightcove.cache.port', isset($config['cache']) && $config['cache']['port'] ? $config['cache']['port'] : null);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
